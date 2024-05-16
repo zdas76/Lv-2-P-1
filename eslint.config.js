c77345bd7f0df27a1ae503f7836261e9f494bfe2
zdas76 <zdas76@gmail.com>
2024-05-16 20:48:13 +0600

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+];
